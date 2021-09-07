33544ccec76fb8a4ead47c3d853a81a97309c278
Andres Asensio Ramos <aasensioramos@gmail.com>
2021-09-07 16:35:30 +0100

    First commit

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# vacunas2021
+# Simple estudio de la tasa máxima esperada de vacunación en España por Comunidad Autónoma
+
+## Uso
+
+Ejecuta el script usando
+
+	python vacunas.py
+
+Este script recogerá los últimos datos de la maravillosa web Datadista (https://github.com/datadista/datasets), hará
+un ajuste por una función logística generalizada y estima, dados los datos, el porcentaje máximo de vacunación. 
+El procedimiento es muy simple y no devuelve incertidumbres por ahora.
+  
---
+++ vacunas.py
@@ -0,0 +1,89 @@
+import numpy as np
+import matplotlib.pyplot as pl
+import pandas as pd
+import scipy.optimize as opt
+import datetime
+import requests
+
+def f(x, a, c, d):
+    return a / (1. + np.exp(-c * (x - d)))
+
+if __name__ == '__main__':
+    print('Actualizando datos.')
+    data = requests.get('https://raw.githubusercontent.com/datadista/datasets/master/COVID%2019/ccaa_vacunas.csv')
+    with open('ccaa_vacunas.csv', 'wb') as file:
+        file.write(data.content)
+
+    tmp = pd.read_csv('ccaa_vacunas.csv')
+
+    values = tmp.loc[tmp['cod_ine'] == 1]['Fecha publicación'].values    
+    last_day = datetime.datetime.strptime(values[-1], '%Y-%m-%d')
+    now = datetime.datetime.now()
+        
+
+    pct_completa = []
+    pct_unadosis = []
+    days = []
+    name = []
+
+    for comunidad in range(19):
+
+        values = tmp.loc[tmp['cod_ine'] == comunidad+1]['Porcentaje con pauta completa'].values
+        values = np.array([float(i.replace(',', '.')) for i in values])
+        pct_completa.append(values)
+
+        values1 = tmp.loc[tmp['cod_ine'] == comunidad+1]['Personas con al menos una dosis'].fillna('0.001').values        
+        values1 = np.array([float(i.replace('.', '')) for i in values1])
+        values2 = tmp.loc[tmp['cod_ine'] == comunidad+1]['Personas con pauta completa'].fillna('0.001').values
+        values2 = np.array([float(i.replace('.', '')) for i in values2]) + 1
+        
+        values = np.nan_to_num(values1 / values2 * values)
+        pct_unadosis.append(values)
+
+        name.append(tmp.loc[tmp['cod_ine'] == comunidad+1]['CCAA'].values[0])
+
+        values = tmp.loc[tmp['cod_ine'] == comunidad+1]['Fecha publicación'].values
+        reference = datetime.datetime.strptime(values[0], '%Y-%m-%d')
+        values = np.array([(datetime.datetime.strptime(values[i], '%Y-%m-%d') - reference).days for i in range(len(values))])
+        days.append(values)
+
+    name = [l.replace('Castilla', 'C.') if 'Castilla' in l else l for l in name]
+    name = [l.replace(' y', '') for l in name]
+    
+
+    with pl.xkcd():
+        fig, ax = pl.subplots(nrows=4, ncols=5, figsize=(17, 13), sharey='row', sharex='col')
+        days_full = np.linspace(0.0, 400, 400)
+        for i in range(19):
+
+            a, c = np.random.exponential(size=2)
+            d = 1.0
+            (a_, c_, d_), _ = opt.curve_fit(f, days[i] / 100.0, pct_completa[i] / 100.0)
+            
+            (a1_, c1_, d1_), _ = opt.curve_fit(f, days[i] / 100.0, pct_unadosis[i] / 100.0)
+
+            y_fit = f(days_full / 100.0, a_, c_, d_)
+
+            if (i == 0):            
+                ax.flat[i].plot(days[i], pct_completa[i], linewidth=3, label='2 dosis')
+                ax.flat[i].plot(days[i], pct_unadosis[i], label='1 dosis')            
+                ax.flat[i].plot(days_full, y_fit * 100.0, ':', linewidth=3, label='Proyección')
+            else:
+                ax.flat[i].plot(days[i], pct_completa[i], linewidth=3)
+                ax.flat[i].plot(days[i], pct_unadosis[i])
+                ax.flat[i].plot(days_full, y_fit * 100.0, ':', linewidth=3)
+
+            ax.flat[i].set_ylim([0, 100])            
+
+            print(f' - {name[i]:15s} -> a={a_:5.2f}, c={c_:5.2f}, d={d_:5.2f}')
+
+            ax.flat[i].set_title(f'{name[i]} - {100.0*a_:3.1f}')        
+
+        ax.flat[-1].remove()
+        fig.supxlabel('Días desde inicio vacunación')
+        fig.supylabel('Porcentaje vacunación completa')
+        fig.suptitle(f"Actualizado a {last_day.strftime('%Y-%m-%d')}")
+        fig.legend(bbox_to_anchor=(0.9,0.25))
+        pl.show()
+
+    pl.savefig('vacunas.png', dpi=300)
